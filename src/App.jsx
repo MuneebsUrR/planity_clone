00315603c46336e3aa9278526ce384b4efb459be
Muneeb Ur Rehman <muneebsurrehman@gmail.com>
2024-08-08 06:26:54 +0500

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import bgImage from './assets/images/home-bg.jpg'; // Adjust the path as needed
 import './App.css';
+import Header from './components/Header';
 const App = () => {
   return (
-   <div className='image'>
-
-   </div>
+    <div className='image'>
+      <Header/>
+    </div>
   );
 };
 
